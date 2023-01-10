2383f5dc10cb3b7c3f51ccd56d0f307cc789e720
mac_haemin <haeminlee@Haeminui-MacBookPro.local>
2023-01-09 21:34:08 -0800

    first lecutre

---
+++ Pyhton Basic/Lecture_1/input.py
@@ -0,0 +1,14 @@
+x = input("Enter 1st Number: ");
+y = input("Enter 2nd Number: ");
+z = int(x) + int(y);
+print(f"addition of x and y will be: {z}");
+print("addition of x and y will be: %s" %z)
+
+# entering using commend line
+# x = int(sys.argv[1]);
+
+ch = input("enter a char: ")[0];
+print(ch);
+
+help(ch);
+type(ch);
---
+++ Pyhton Basic/Lecture_1/range.py
@@ -0,0 +1,9 @@
+friends = ["abby", "Jonathon", "Becky", "Ryan"]
+
+# end = "" will make print doesn't change line.
+for friend in friends:
+    print(friend, end = " ");
+
+for i in range(10, 15, 30):
+    print(i)
+
