24bcb3f423c0e541299bdbdbd94432b025ce1fe2
Haemin <leehaemin9014@gmail.com>
2023-02-13 21:44:43 -0800

    didn't have time today, had study little

---
+++ Udemy/Python BootCamp/Lecture_4/random_module.py
@@ -0,0 +1,8 @@
+import random;
+import my_module;
+random_integer = random.randint(1, 10);
+random_float = random.random();
+print(random_integer);
+print(random_float);
+print(round(random_float * 5 + 1));
+print(my_module.pi);
