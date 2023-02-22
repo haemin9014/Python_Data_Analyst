84504f5811cbb1cdbaa88d7b5fe0046bfe7d35fa
Haemin <leehaemin9014@gmail.com>
2023-02-21 22:12:50 -0800

    day 6

---
+++ Udemy/Python BootCamp/Lecture_6/Indentation.py
@@ -0,0 +1,5 @@
+#taking care the line is super example in python.(just like file structure)
+def my_function():
+    print("hello");
+
+#python3 recommend 4space for indentation.
--- Udemy/Python BootCamp/Lecture_6/defining_and_calling_functions.py
+++ Udemy/Python BootCamp/Lecture_6/defining_and_calling_functions.py
@@ -1,3 +1,10 @@
 #defining function
 def my_function():
     print("Hello, this is my first basic function");
+
+my_function();
+
+def used_object(o):
+    print(f"We are printing out {o}");
+
+used_object(1);
